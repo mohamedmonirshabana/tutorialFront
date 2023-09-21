2de60e6295f4cc4e664607f82bf87f36c12a1a64
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-09-22 00:24:51 +0300

    start react project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// import "./App.css";
 
 function App() {
   return (
